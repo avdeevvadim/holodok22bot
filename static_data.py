5c5eaceaa5aca2ef10f045795d80d4aca38a89a5
Vadim <avdeev.vadim@gmail.com>
2022-10-03 22:49:04 +0300

    Create static_data.py

---
+++ static_data.py
@@ -0,0 +1,218 @@
+camps_data = [
+    ('¡Останови мир!', 'https://telegra.ph/Ostanovi-mir-02-19'),
+    ('89 историй', 'https://telegra.ph/89-istorij-02-19'),
+    ('Aftorehab', 'https://telegra.ph/Aftorehab-02-19'),
+    ('Backstreet Bots', 'https://telegra.ph/Backstreet-Bots-02-19'),
+    ('Brunch House Mafia', 'https://telegra.ph/Brunch-House-Mafia-02-19'),
+    ('Drag Dealers: Mercury in Uranus', 'https://telegra.ph/Drag-Dealers-Mercury-in-Uranus-02-19'),
+    ('Gipsy camp: ComMOONa', 'https://telegra.ph/Gipsy-camp-ComMOONa-02-19'),
+    ('Glitter it up!', 'https://telegra.ph/Glitter-it-up-02-19'),
+    ('Good Morning Run', 'https://telegra.ph/Good-Morning-Run-02-21'),
+    ('Jazz & Wine', 'https://telegra.ph/Jazz--Wine-02-19'),
+    ('Kasha Akashi Meditation Bar', 'https://telegra.ph/Kasha-Akashi-Meditation-Bar-02-19'),
+    ('LAGER’FEИ☘️❤️🧚🏻♀️', 'https://telegra.ph/LAGERFEI-02-19'),
+    ('PINK POLICE', 'https://telegra.ph/PINK-POLICE-02-19'),
+    ('Pink Space Bar', 'https://telegra.ph/Pink-Space-Bar-02-19'),
+    ('Sacred Garden', 'https://telegra.ph/Sacred-Garden-02-19'),
+    ('Saint Nudes', 'https://telegra.ph/Saint-Nudes-02-19'),
+    ('Skolopendra bar', 'https://telegra.ph/Skolopendra-bar-02-19'),
+    ('Slomoth', 'https://telegra.ph/Slomoth-02-19'),
+    ('YASNO.PRO', 'https://telegra.ph/YASNOPRO-02-19'),
+    ('Yeti-встретит', 'https://telegra.ph/Yeti-vstretit-02-19'),
+    ('Баня Будит | Самоцветье', 'https://telegra.ph/Banya-Budit--Samocvete-02-19'),
+    ('Бар «Л’ Отребье»', 'https://telegra.ph/Bar-L-Otrebe-02-19'),
+    ('Бьютифул Бар «Гламурия»', 'https://telegra.ph/Byutiful-Bar-Glamuriya-02-19'),
+    ('Бюро путешествий “Ромашка”', 'https://telegra.ph/Byuro-puteshestvij-Romashka-02-19'),
+    ('в Темноте', 'https://telegra.ph/v-Temnote-02-19'),
+    ('Ветер, который срывает кожу', 'https://telegra.ph/Veter-kotoryj-sryvaet-kozhu-02-19'),
+    ('Водоворот эмоций', 'https://telegra.ph/Vodovorot-ehmocij-02-19'),
+    ('Гедонисты', 'https://telegra.ph/Gedonisty-02-19'),
+    ('ГНИ!', 'https://telegra.ph/GNI-02-21'),
+    ('Да!О, Баня 2.0.22 - Калейдоскоп состояний', 'https://telegra.ph/DaO-Banya-2022---Kalejdoskop-sostoyanij-02-19'),
+    ('ДЕТСКАЯ', 'https://telegra.ph/DETSKAYA-02-19'),
+    ('Закрутить?', 'https://telegra.ph/Zakrutit-02-19'),
+    ('Залезь в меня', 'https://telegra.ph/Zalez-v-menya-02-21'),
+    ('Зендо', 'https://telegra.ph/Zendo-02-19'),
+    ('Иллюзионисты', 'https://telegra.ph/Illyuzionisty-02-19'),
+    ('Иммерсивный спектакль «Космос во мне»', 'https://telegra.ph/Immersivnyj-spektakl-Kosmos-vo-mne-02-19'),
+    ('Импровь-Церковь', 'https://telegra.ph/Improv-Cerkov-02-19'),
+    ('Классика', 'https://telegra.ph/Klassika-02-21'),
+    ('Ко-Лобок', 'https://telegra.ph/Ko-Lobok-02-19'),
+    ('Костюм Кэмп “Блеск и Бластер”', 'https://telegra.ph/Kostyum-Kehmp-Blesk-i-Blaster-02-19'),
+    ('Любовники', 'https://telegra.ph/Lyubovniki-02-19'),
+    ('Мандала. Мандаласкоп', 'https://telegra.ph/Mandala-Mandalaskop-02-19'),
+    ('Млечный путь', 'https://telegra.ph/Mlechnyj-put-02-19'),
+    ('МОРЕ: песочница', 'https://telegra.ph/MORE-pesochnica-02-19'),
+    ('НАпамять', 'https://telegra.ph/NApamyat-02-19'),
+    ('Несегодня', 'https://telegra.ph/Nesegodnya-02-19'),
+    ('Несу хуйню', 'https://telegra.ph/Nesu-hujnyu-02-19'),
+    ('Новая Жизнь, проект "Undina"', 'https://telegra.ph/Novaya-ZHizn-proekt-Undina-02-19'),
+    ('ОгонИя', 'https://telegra.ph/OgonIya-02-21'),
+    ('Одна на крыше ⧊', 'https://telegra.ph/Odna-na-kryshe-02-19'),
+    ('Передвижная фондюшница FLy Flo', 'https://telegra.ph/Peredvizhnaya-fondyushnica-FLy-Flo-02-19'),
+    ('Пиратское радио ШалашFM', 'https://telegra.ph/Piratskoe-radio-SHalashFM-02-19'),
+    ('ПогребальНЯ и Душный бар', 'https://telegra.ph/PogrebalNYA-i-Dushnyj-bar-02-19'),
+    ('Получил', 'https://telegra.ph/Poluchil-02-19'),
+    ('Потаскушка', 'https://telegra.ph/Potaskushka-02-19'),
+    ('Почтовая служба "Вездеходы"', 'https://telegra.ph/Pochtovaya-sluzhba-Vezdehody-02-19'),
+    ('Привкус кармы', 'https://telegra.ph/Privkus-karmy-02-19'),
+    ('Пряничная', 'https://telegra.ph/Pryanichnaya-02-19'),
+    ('Рейнджеры', 'https://telegra.ph/Rejendzhery-02-19'),
+    ('Рэп Четверг a.k.a DUB SREDA', 'https://telegra.ph/Rehp-CHetverg-aka-DUB-SREDA-02-19'),
+    ('Сани Бич', 'https://telegra.ph/Sani-Bich-02-19'),
+    ('Сани с Усами', 'https://telegra.ph/Sani-s-Usami-02-19'),
+    ('Светлица', 'https://telegra.ph/Svetlica-02-19'),
+    ('Сказочная сила', 'https://telegra.ph/Skazochnaya-sila-02-19'),
+    ('Спермосдаточная - достаточно ли?', 'https://telegra.ph/Spermosdatochnaya---dostatochno-li-02-19'),
+    ('Темпл «Сияние чистого разума»', 'https://telegra.ph/Templ-Siyanie-chistogo-razuma-02-19'),
+    ('Тепло', 'https://telegra.ph/Teplo-02-19'),
+    ('ТОЧКА ЧЁРНОГО', 'https://telegra.ph/TOCHKA-CHYORNOGO-02-21'),
+    ('Фито Бро', 'https://telegra.ph/Fito-Bro-02-19'),
+    ('Фотоувеличение впечатления', 'https://telegra.ph/Fotouvelichenie-vpechatleniya-02-19'),
+    ('Цыганский Табор', 'https://telegra.ph/Cyganskij-Tabor-02-19'),
+    ('Шар-Пицца', 'https://telegra.ph/SHar-Picca-02-22'),
+    ('Эдем Иштар', 'https://telegra.ph/EHdem-Ishtar-02-19'),
+    ('Экоголики', 'https://telegra.ph/EHkogoliki-02-19')
+]
+
+memes_data = [
+    ('PHOTO', 'AgACAgIAAxkBAAIHMWITjoOSwH7r2XspFve4WPVvpODUAAK1tzEb7xyZSBAj1xcH-4_0AQADAgADcwADIwQ', 'С этим сложно поспорить, конечно!'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHM2ITjqeHen2FRVPbrGWkXdT-I82WAAK2tzEb7xyZSLD4j7lKk8XlAQADAgADcwADIwQ', 'Просто мы купили нативную рекламу…'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHNWITjtvP75l1x2yRCT8JDJ3VKC47AAK3tzEb7xyZSIcT6a0Z5dAsAQADAgADcwADIwQ', 'Про это шутить не будем, пожалуй'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHN2ITju_laBI6HPXWDpbrebtOBAdTAAK4tzEb7xyZSIspnQwsFHu8AQADAgADcwADIwQ', 'Не забывайте откладывать 10 процентов с каждой зарплаты!'),
+    ('ANIMATION', 'CgACAgQAAxkBAAIHOWITj1uWLc89VAaYHsYJTFedR_tgAALNAgACdjm0UpFhxYSE50g0IwQ', 'Муд 😎'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHTmIT6ZUGbfMo_2q7Ty46Qscg5wowAAJjuDEbrjKQSFt-sHU92OFEAQADAgADcwADIwQ', 'Ситуация Холодок!'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHWWIT6zI2LSubVoZ27pTdl7qzfCAnAAJ5uDEbrjKQSBzTU4F3iApoAQADAgADcwADIwQ', 'Принарядились?'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHW2IT60qk8CfWjv7wDc7K0JkxkDPoAAJ6uDEbrjKQSLXKOnsdQsdtAQADAgADcwADIwQ', 'Приходи в Погребальню пообщаться с шаманом, пока до психиатра не дозвониться'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHXWIT62axDY1V2s4uXW57m28FKXLTAAJ7uDEbrjKQSL37IKv2e9LhAQADAgADcwADIwQ', 'Ты там как, живой?'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHX2IT63u6T_97abYQMBws9DrE43zvAAJ8uDEbrjKQSG6ntoPOpwwUAQADAgADcwADIwQ', 'Нет, нельзя'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHYWIT65GxbiKyoBJw6uY525vDes_PAAJ9uDEbrjKQSNDtqYqZHQixAQADAgADcwADIwQ', 'Найди локацию Погребальни по рейв-кладбищу'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHY2IT66nNGQO1MXYnIfR_6mljvrP5AAJ-uDEbrjKQSCvn1L1D96SpAQADAgADcwADIwQ', 'ЕЕЕЕ, продолжаем веселиться!'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHZWIT68OstKSH6Qig-izFjapnPjH_AAJ_uDEbrjKQSL-7LR6u4LDtAQADAgADcwADIwQ', 'Очень насыщенная программа у Холодка…'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHZ2IT69iS04VFxt-VbO0qNVwxbOJ5AAKAuDEbrjKQSJQb4LQfQrLdAQADAgADcwADIwQ', 'Наш тип вечеринки. Приходи погребаться!'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHaWIT6-tFsyebA9ctMKh0854Zw70qAAKBuDEbrjKQSMFDjId_Tv_hAQADAgADcwADIwQ', 'Когда новости смешнее любого мема'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHa2IT7APdwGHbW92XKwpNkEm-qrUDAAKCuDEbrjKQSGohPg_ujvEnAQADAgADcwADIwQ', 'Не кисни там, бро/сис'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHbWIT7BXEhKr2bYci11JUVDeR-2Z1AAKDuDEbrjKQSPLsa8jab4AIAQADAgADcwADIwQ', 'Если хочешь помереть – приходи, а если хочешь поговорить об этом – у нас будет воркшоп по написанию последнего поста'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHb2IT7CmqONE3JFpIfXm-8AABV2AZOAAChLgxG64ykEiTaFZNSjqlXgEAAwIAA3MAAyME', 'Актуально последние пару дней'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHcWIT7El0GBPgYcyAJM8TSPO1OZt8AAKFuDEbrjKQSLJ5LxfXJlVoAQADAgADcwADIwQ', 'У нас будет лекция про Смерть в кино, ждем тебя!'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHc2IT7Ft1czO-hCqoeyRSZP_hAS4XAAKGuDEbrjKQSF6kcfvKQe9MAQADAgADcwADIwQ', 'Тема Погребальни в этот раз – Цифровая смерть'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHdWIT7G141oB0j-2jOIAK6yegwJVzAAKHuDEbrjKQSCXbI3oTZCLsAQADAgADcwADIwQ', 'Классика'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHd2IT7H2zPeaWm0RaZN7Ai9WzZ7pWAAKIuDEbrjKQSCR90PTjU4gsAQADAgADcwADIwQ', 'Под этим мемом анонсируем вокальный мастер-класс. Извините'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHeWIT7I2JPRKp7FYue5ufUztmyzBCAAKJuDEbrjKQSBodzUIGbx_NAQADAgADcwADIwQ', 'Не замерзли?'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHe2IT7KBjpygRuIh1P_YY9ks0FKscAAKKuDEbrjKQSDlir9PmmZyUAQADAgADcwADIwQ', 'У нас будет лекция про смерть в искусстве, поэтому вот вам концептуальный мем'),
+    ('PHOTO', 'AgACAgIAAxkBAAIHfWIT7LG8sF7aUZdohmRm92I_LlJdAAKLuDEbrjKQSN4JzXjbWP_6AQADAgADcwADIwQ', 'Приходи в Погребальню, обсудим мирское'),
+    ('ANIMATION', 'CgACAgQAAxkBAAIHjGIT7p6a8UtiqTWVUHCrpUsohfMaAAI0AwACLqa8Ums_s_Y_xzcnIwQ', 'Как твоя вечеринка?'),
+    ('ANIMATION', 'CgACAgQAAxkBAAIHjmIT7sRJQ3vT4JNhLUAuvqJTZgQhAALeAgACL_i9UlQ_zE9JyWv5IwQ', ''),
+    ('ANIMATION', 'CgACAgQAAxkBAAIHkGIT7vWrksZDy2thKemt6RLAICAuAAIZAwACG6e1Us2WnNXmMQdBIwQ', 'Всё такое красивое'),
+    ('ANIMATION', 'CgACAgQAAxkBAAIHkmIT7yK5rEw39WSXqr9LW3UXKfJJAAIiAwAC-ZzNUnraG1d4MsR4IwQ', 'Ждем тебя на нашей вечернике'),
+    ('ANIMATION', 'CgACAgQAAxkBAAIHlGIT70r7YoiMybNTxY30Y66BXJHYAAI4AwACvIe9UhE5be7MZLnJIwQ', 'Шаман Погребальни в мире котов'),
+    ('ANIMATION', 'CgACAgQAAxkBAAIHlmIT74_r3V42fPCjvvZAdCaSoQl3AAImAwACHvC1Ug4DCLXVp-pNIwQ', 'Мы поднимем тебе настроение'),
+    ('ANIMATION', 'CgACAgQAAxkBAAIHmGIT77cxO3cBD0L0nzOMmOSlIqbTAAIrAwACYFi0Uh-SaafRZIg5IwQ', ''),
+    ('ANIMATION', 'CgACAgQAAxkBAAIHmmIT8BvQlem_9ermLyM3IMkcqllLAALlAgACf4G9Ug6039zwNz2DIwQ', ''),
+    ('ANIMATION', 'CgACAgQAAxkBAAIHmmIT8BvQlem_9ermLyM3IMkcqllLAALlAgACf4G9Ug6039zwNz2DIwQ', ''),
+    ('ANIMATION', 'CgACAgQAAxkBAAIHnGIT8EYxeiTrw65VuZ6pUIHYdpFbAAIVAwACqa-0UqhZXrayzsxsIwQ', 'У нас радостно!'),
+]
+
+principles_description = (
+    '<b>Принципы, по которым живёт наше сообщество</b>:\n\n'
+    '1. Радикальное включение\n\n'
+    '2. Дарение\n\n'
+    '3. Декоммодификация\n\n'
+    '4. Радикальная самодостаточность\n\n'
+    '5. Радикальное самовыражение\n\n'
+    '6. Общественные усилия\n\n'
+    '7. Гражданская ответственность\n\n'
+    '8. Не оставляй следа\n\n'
+    '9. Участие\n\n'
+    '10. Здесь и сейчас\n\n'
+    'Помнишь, что означает каждый из них?'
+)
+
+principles_data = [
+    '1. <b>Радикальное включение</b>\n\n'
+    'Радикальное включение значит, что любой человек может стать частью сообщества Russian Burners. '
+    'Мы делаем мероприятия, где у каждого есть шанс найти себя и своих единомышленников. На них становятся не важны ваши политические взгляды, '
+    'возраст, фигура, внешность, финансовые возможности, язык и любые другие разделения.\n\nМы рады и открыты новым участникам идеям и концепциям. '
+    'Мы не делаем исключений и не ставим никаких ограничений для вашего участия.\n\n'
+    '<b>Radical Inclusion</b>\n\n'
+    'Anyone may be a part of Burning Man. We welcome and respect the stranger. No prerequisites exist for participation in our community.',
+
+    '2. <b>Дарение</b>\n\n'
+    'На мероприятиях сообщества Burning man нет коммерции и деньги не в ходу. Здесь действует принцип дарения и благодарности между участниками. '
+    'Кто-то готовит заранее маленькие подарки, чтобы сказать спасибо. Кто-то строит большой арт-объект или устраивает какую-то тематическую '
+    'активность в подарок всем. А кто-то старательно наряжается в невообразимый костюм — и дарит радость от красоты и восторга окружающим. Ищите '
+    'свой способ выразить благодарность!\n\n'
+    '<b>Gifting</b>\n\n'
+    'Burning Man is devoted to acts of gift giving. The value of a gift is unconditional. Gifting does not contemplate a return or an exchange '
+    'for something of equal value.',
+
+    '3. <b>Декоммодификация</b>\n\nГде нет коммерции, там нет места и рекламным активностям.\n\nЗдесь вы не увидите шатры с огромными логотипами '
+    'компаний. А участники заклеивают или забавно обыгрывают бренды, которые указаны даже на одежде. Это в итоге создает невероятное впечатление '
+    'полной оторванности от дефолтного мира, где мы уже даже не замечаем количество рекламы, с которым сталкиваемся каждый день.\n\n'
+    '<b>Decommodification</b>\n\n'
+    'In order to preserve the spirit of gifting, our community seeks to create social environments that are unmediated by commercial sponsorships, '
+    'transactions, or advertising. We stand ready to protect our culture from such exploitation. We resist the substitution of consumption for '
+    'participatory experience.',
+
+    '4. <b>Радикальная самодостаточность</b>\n\nКаждый участник должен рассчитывать только на себя, чтобы обеспечить себе комфорт и впечатления. '
+    'Это не значит, что вам не помогут! Открытость и взаимовыручка также очень важны. Однако именно независимость каждого участника создает здоровое '
+    'сообщество и позволяет организовывать мероприятия Burning man в самых разных условиях. Будь то жаркая пустыня в штате Невада или заснеженное '
+    'поле в Подмосковье. С опорой на самого себя вам удастся получить намного более сильные впечатления.\n\n'
+    '<b>Radical Self-reliance</b>\n\n'
+    'Burning Man encourages the individual to discover, exercise and rely on their inner resources.',
+
+    '5. <b>Радикальное самовыражение</b>\n\nСкажем просто — это самое безопасное место для вашего исследования себя, поэтому не ограничивайся в '
+    'выборе. Попробуйте образы, которые никогда не надевали, расскажите о себе миру с помощью простых или сложных инструментов. Не бойтесь, если '
+    'идеи придут к вам не сразу, в итоге вы все равно разберетесь. Но точно не стоит пропускать этот опыт. Это будет подарком и самому себе и '
+    'окружающим.\n\n'
+    '<b>Radical Self-expression</b>\n\n'
+    'Radical self-expression arises from the unique gifts of the individual. No one other than the individual or a collaborating group can determine '
+    'its content. It is offered as a gift to others. In this spirit, the giver should respect the rights and liberties of the recipient.',
+
+    '6. <b>Общественные усилия</b>\n\nЕсли кратко, то можно сформулировать это как «делу — время, а потехе — час». Все, что происходит на '
+    'мероприятиях сообщества Burning man делают не нанятые работники, а такие же участники как вы. Придумывают. Строят. Создают. Украшают. А потом, '
+    'конечно же, от души отдыхают и веселятся все вместе. Постарайтесь принять участие в общей работе, узнайте в сообществе, какая помощь нужна, '
+    'узнайте, есть ли на мероприятии департамент волонтеров и как туда вступить. Помощь нужна всегда.\n\n'
+    '<b>Communal Effort</b>\n\n'
+    'Our community values creative cooperation and collaboration. We strive to produce, promote and protect social networks, public spaces, works '
+    'of art, and methods of communication that support such interaction.',
+
+    '7. <b>Гражданская ответственность</b>\n\nПринцип «Гражданская ответственность» о ценности гражданского общества и его законов.\n\nУчастники '
+    'сообщества принимают на себя ответственность за общественную безопасность, а также обязаны следить за соблюдением государственных законов.\n\n'
+    'Вместе с ростом мероприятия, мы привлекли к работе сторонние службы, которые помогают охранять вас и ваше комфортное пребывание от нежданных '
+    'гостей и чрезвычайных происшествий. Вместе с ними на Огоньке всегда дежурят рейнджеры. Это опытные участники сообщества, которые в этом году '
+    'взяли на себя ответственность следить за комфортом и безопасностью.\n\nМы настоятельно просим вас не нарушать законодательство РФ. А также '
+    'призываем всех участников оказывать содействие в работе рейнджеров и действовать согласно их указаниям. Это очень поможет в случае '
+    'возникновения ситуации, требующей оперативного реагирования.\n\n'
+    '<b>Civic Responsibility</b>\n\n'
+    'We value civil society. Community members who organize events should assume responsibility for public welfare and endeavor to communicate '
+    'civic responsibilities to participants. They must also assume responsibility for conducting events in accordance with local, state and '
+    'federal laws.',
+
+    '8. <b>Не оставляй следа</b>\n\nЭто значит, что нужно оставить место, в котором мы строили, веселились и менялись, в том же виде, каким оно '
+    'было, когда мы пришли. Принцип родился в связи с особенностями проведения больших Бернов — обычно они проходят в заповедных зонах или других '
+    'природных местах, где нельзя никак влиять на окружающий мир. Но он был перенят и распространен на все мероприятия, даже если они проходят на '
+    'территории заводов или просто в баре. Скажем больше, когда вы попробуете пожить с этим принципом неделю, вы больше не сможете '
+    'перестроиться.\n\n'
+    '<b>Leaving No Trace</b>\n\n'
+    'Our community respects the environment. We are committed to leaving no physical trace of our activities wherever we gather. We clean up '
+    'after ourselves and endeavor, whenever possible, to leave such places in a better state than when we found them.',
+
+    '9. <b>Участие</b>\n\nИдея в том, что вы не просто гость, сторонний наблюдатель или «турист». Если вы решаетесь присоединиться к сообществу '
+    'или прийти на мероприятие, то вы — участник. Вы — часть той радости и праздника, который происходит, потому что такие же как вы приложили к '
+    'этому максимум усилий. Постарайтесь найти свою роль и включиться в процесс. Любая роль важна, даже самая простая или кажущаяся '
+    'незаметной.\n\n'
+    '<b>Participation</b>\n\n'
+    'Our community is committed to a radically participatory ethic. We believe that transformative change, whether in the individual or in '
+    'society, can occur only through the medium of deeply personal participation. We achieve being through doing. Everyone is invited to work. '
+    'Everyone is invited to play. We make the world real through actions that open the heart.',
+
+    '10. <b>Здесь и сейчас</b>\n\nВы едете на любое мероприятие под грифом Burning man с великими планами и чаще всего делаете что-то совсем '
+    'другое. Вы сталкиваетесь с магией места, которая «лучше знает», какой опыт вам нужно прожить именно сейчас. Отличное упражнение по '
+    'стабилизации в моменте особенно для контрол-фриков! Мучительно и прекрасно. Тут главное не сопротивляться, а останавливаться и стараться '
+    'поймать это «здесь и сейчас». И будь что будет!\n\n'
+    '<b>Immediacy</b>\n\n'
+    'Immediate experience is, in many ways, the most important touchstone of value in our culture. We seek to overcome barriers that stand '
+    'between us and a recognition of our inner selves, the reality of those around us, participation in society, and contact with a natural '
+    'world exceeding human powers. No idea can substitute for this experience.'
+]
